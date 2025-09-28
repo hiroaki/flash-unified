6cd452b63198cbb253a298a91cfba777add79593
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-09-28 13:39:44 +0900

    feat: add flash message functionality and update routes for success and failure actions

--- test/dummy/app/controllers/dummies_controller.rb
+++ test/dummy/app/controllers/dummies_controller.rb
@@ -3,8 +3,11 @@ class DummiesController < ApplicationController
   end
 
   def success
+    redirect_to dummies_path, notice: "Saved successfully"
   end
 
   def failure
+    flash.now[:alert] = "Could not create."
+    render :index
   end
 end
